358cec48f2f0906b03c6747c4a78f9875f958c3a
vivekgandharla <sonugandherla01@gmail.com>
2023-07-03 23:54:11 +0530

    1

---
+++ src/com/gfgcip/Main.java
@@ -0,0 +1,15 @@
+package com.gfgcip;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Escape sequence
+        System.out.println("c:\\xyz\\abc.txt");
+        System.out.println("hey\nim viv");
+        System.out.println("hii\tyou");
+        System.out.println("my name is \"vivek\"");
+        System.out.println("hey if i write 'viv'");
+    }
+}
